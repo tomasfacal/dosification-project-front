5c091525b85eb0e6159f28ca88ef00e8147c0531
Agustin Pirotto <aguspirotto96@gmail.com>
2021-11-18 22:19:30 -0300

    wip crear paciente

--- src/features/create-patient/create-patient.tsx
+++ src/features/create-patient/create-patient.tsx
@@ -1,9 +1,14 @@
 import React, {Fragment, useState} from 'react';
 import styles from './create-patient.module.scss';
+import { TextField, Tooltip, Button, FormControl, 
+         FormLabel, FormControlLabel, RadioGroup, Radio
+        } from '@material-ui/core';
 
 const CreatePatient = () => {
     const [datos, setData] = useState({
-        name: ''
+        name: '',
+        document_number: '',
+        sex: 'female',
     })
 
     const handleInputChange = (event: any) => {
@@ -16,20 +21,51 @@ const CreatePatient = () => {
     const sendData = (event: any) => {
         event.preventDefault()
         console.log('enviando datos...' + datos.name)
+        alert('se van a enviar maquinas')
     }
 
     return (
         <Fragment>
-            <h1 className= {styles.manu} >Crear Paciente</h1>
-            <form onSubmit={sendData}>
-                <div>
-                    <input type="text" placeholder="Nombre"  onChange={handleInputChange} name="name"></input>
-                </div>
-                <button type="submit">Enviar</button>
-            </form>
-            <ul>
-                <li>{datos.name}</li>
-            </ul>
+          <h1 className= {styles.Title} >Crear Paciente</h1>
+            <div className= {styles.fieldContainer}>
+              <TextField
+                label="Nombre"
+                name= "name"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className= {styles.fieldContainer}>
+            <Tooltip title="Cedula con digito verificador, sin puntos ni guiones" arrow>
+              <TextField
+                  type="number"
+                  label="Cédula de identidad"
+                  name= "document_number"
+                  placeholder= "49305483"
+                  onChange={handleInputChange}
+                />
+            </Tooltip>
+            </div>
+            <div className= {styles.fieldContainer}>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">Género</FormLabel>
+                <RadioGroup
+                  aria-label="gender"
+                  defaultValue="female"
+                  name="sex"
+                  onChange={handleInputChange}
+                >
+                  <FormControlLabel value="female" control={<Radio />} label="Femenino" />
+                  <FormControlLabel value="male" control={<Radio />} label="Masculino" />
+                </RadioGroup>
+              </FormControl>
+            </div>
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={sendData}
+            >
+              Crear Paciente
+            </Button>
         </Fragment>
     );
 }
