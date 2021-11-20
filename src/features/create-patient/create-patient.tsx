c1942b07932113a3b7edaaae43adc9e44db8e2eb
Agustin Pirotto <aguspirotto96@gmail.com>
2021-11-20 17:05:08 -0300

    Create patient form

--- src/features/create-patient/create-patient.tsx
+++ src/features/create-patient/create-patient.tsx
@@ -1,15 +1,18 @@
 import React, {Fragment, useState} from 'react';
 import styles from './create-patient.module.scss';
 import { TextField, Tooltip, Button, FormControl, 
-         FormLabel, FormControlLabel, RadioGroup, Radio
+         FormLabel, FormControlLabel, RadioGroup, Radio, Grid, Typography
         } from '@material-ui/core';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 const CreatePatient = () => {
     const [datos, setData] = useState({
         name: '',
         document_number: '',
         sex: 'female',
-    })
+        lastname: '',
+        errorMessage: '',
+    }) 
 
     const handleInputChange = (event: any) => {
         setData({
@@ -18,54 +21,94 @@ const CreatePatient = () => {
         })
     }
 
-    const sendData = (event: any) => {
-        event.preventDefault()
-        console.log('enviando datos...' + datos.name)
-        alert('se van a enviar maquinas')
+    const validateFields = (name: string, lastname: string, document_number: string, sex: string) => (
+      (!!name && !!lastname && !!document_number && !!sex)
+    );
+
+    const handleSubmit = (event: any) => {
+      event.preventDefault();
+      const bool = [true, false];
+      const random = Math.floor(Math.random() * bool.length);
+      const result = bool[random]
+      console.log(result);
+      if (!result) {
+        setData({...datos, errorMessage: 'Error al crear paciente'})
+      } else {
+        setData({...datos, errorMessage: ''})
+        console.log('enviando datos...' + datos.name);
+        alert('se van a enviar datos al back');
+      }
     }
 
     return (
         <Fragment>
-          <h1 className= {styles.Title} >Crear Paciente</h1>
-            <div className= {styles.fieldContainer}>
-              <TextField
-                label="Nombre"
-                name= "name"
-                onChange={handleInputChange}
-              />
-            </div>
-            <div className= {styles.fieldContainer}>
-            <Tooltip title="Cedula con digito verificador, sin puntos ni guiones" arrow>
-              <TextField
-                  type="number"
-                  label="Cédula de identidad"
-                  name= "document_number"
-                  placeholder= "49305483"
-                  onChange={handleInputChange}
-                />
-            </Tooltip>
-            </div>
-            <div className= {styles.fieldContainer}>
-              <FormControl component="fieldset">
-                <FormLabel component="legend">Género</FormLabel>
-                <RadioGroup
-                  aria-label="gender"
-                  defaultValue="female"
-                  name="sex"
-                  onChange={handleInputChange}
-                >
-                  <FormControlLabel value="female" control={<Radio />} label="Femenino" />
-                  <FormControlLabel value="male" control={<Radio />} label="Masculino" />
-                </RadioGroup>
-              </FormControl>
-            </div>
-            <Button
-              color="primary"
-              variant="contained"
-              onClick={sendData}
-            >
-              Crear Paciente
+          <div className= {styles.FormContainer} >
+            <h1 className= {styles.Title} >Crear Paciente</h1>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <div className= {styles.fieldContainer}>
+                    <TextField
+                      label="Nombre"
+                      name= "name"
+                      error={datos.name === ""}
+                      helperText={datos.name === "" ? 'Nombre requerido' : ' '}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <div className= {styles.fieldContainer}>
+                    <TextField
+                      label="Apellido"
+                      name= "lastname"
+                      error={datos.lastname === ""}
+                      helperText={datos.lastname === "" ? 'Apellido requerido' : ' '}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                </Grid>
+              </Grid>
+              <div className= {styles.fieldContainer}>
+              <Tooltip title="Cedula con digito verificador, sin puntos ni guiones" arrow>
+                <TextField
+                    type="number"
+                    label="Cédula de identidad"
+                    name= "document_number"
+                    placeholder= "49305483"
+                    error={datos.document_number === ""}
+                    helperText={datos.document_number === "" ? 'Cedula de identidad requerida' : ' '}
+                    onChange={handleInputChange}
+                  />
+              </Tooltip>
+              </div>
+              <div className= {styles.fieldContainer}>
+                <FormControl component="fieldset">
+                  <FormLabel component="legend">Género</FormLabel>
+                  <RadioGroup
+                    aria-label="gender"
+                    defaultValue="female"
+                    name="sex"
+                    onChange={handleInputChange}
+                  >
+                    <FormControlLabel value="female" control={<Radio color="primary" />} label="Femenino" />
+                    <FormControlLabel value="male" control={<Radio color="primary" />} label="Masculino" />
+                  </RadioGroup>
+                </FormControl>
+              </div>
+              <Button
+                color="primary"
+                variant="contained"
+                onClick={handleSubmit}
+                className={styles.SubmitButton}
+                disabled={!validateFields(datos.name, datos.lastname, datos.document_number, datos.sex)}
+              >
+                <PersonAddIcon className= {styles.AddClientIcon}/>
+                Crear Paciente
             </Button>
+            {datos.errorMessage && (
+              <Typography className={styles.error}>{datos.errorMessage}</Typography>
+            )}
+          </div>
         </Fragment>
     );
 }
