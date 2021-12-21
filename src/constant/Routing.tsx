9e59abbd37fee90d86d1d437de37a8179f269ba7
Agustin Pirotto <aguspirotto96@gmail.com>
2021-12-20 21:50:29 -0300

    fixes

--- src/constant/Routing.tsx
+++ src/constant/Routing.tsx
@@ -2,12 +2,6 @@ export const Routing = {
     SELECCIONAR_PACIENTE: '/seleccionar_paciente',
     CREATE_PATIENT: '/create-patient',
     SELECT_TREATMENTS: '/select-treatments',
-<<<<<<< HEAD
     COUNTER: '/counter',
-<<<<<<< HEAD
-=======
->>>>>>> wip
-=======
     LIST_PATIENTS: '/list-patients'
->>>>>>> Vista para listar pacientes
 }
