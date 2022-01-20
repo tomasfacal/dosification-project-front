c17866bf2f8aaca658a9324d9bbbf20630d711ab
tomasfacal <tomasfacal97@gmail.com>
2022-01-19 21:25:51 -0300

    Fix strange bug by wrapping the Box component with a FormControl to display Select options correctly in SelectPatientModel

--- src/features/navbar/navbar.tsx
+++ src/features/navbar/navbar.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
-import styles from './navbar.module.scss';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import MedicalServicesIcon from "@mui/icons-material/MedicalServices";
+import styles from "./navbar.module.scss";
+import { FormControl } from "@material-ui/core";
 
 const Navbar = () => {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar className= {styles.NavBarContainer} variant="dense">
-          <MedicalServicesIcon className= {styles.MedicalIcon}/>
-          <Typography variant="h6" color="inherit" component="div">
-            FINGLIX
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <FormControl fullWidth>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar className={styles.NavBarContainer} variant="dense">
+            <MedicalServicesIcon className={styles.MedicalIcon} />
+            <Typography variant="h6" color="inherit" component="div">
+              FINGLIX
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </FormControl>
   );
-}
+};
 
 export default Navbar;
