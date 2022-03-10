eecf4019f4d9ee71e641557606f7ec6f59293b26
Agustin Pirotto <aguspirotto96@gmail.com>
2022-03-09 21:44:29 -0300

    Agrega card de simular y componentiza

---
+++ src/features/feature-card/feature-card.tsx
@@ -0,0 +1,55 @@
+import { Fragment } from "react";
+import styles from "./feature-card.module.scss";
+import { Routing } from "../../constant/Routing";
+import { Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+interface Buttons {
+  buttonText: string;
+  buttonLink: string;
+}
+
+interface Feature {
+  title: string;
+  subtitle: string;
+  image: string;
+  buttons: {
+    buttonText: string;
+    buttonLink: string;
+    buttonStyle: string;
+  }[];
+}
+
+const FeatureCard = (props: Feature) => {
+  return (
+    <Fragment>
+      <div className={styles.PatientsContainer}>
+        <div className={styles.HomeSectionTitle}>{props.title}</div>
+        <div className={styles.PatientsInfo}>
+          <h2>{props.subtitle}</h2>
+          <div className={styles.ImageContainer}>
+            <img
+              src={props.image}
+              alt="ilustracion simulacion"
+              className={styles.PatientImage}
+            />
+            <div className={styles.ButtonsContainer}>
+              {props.buttons.map((button: any) => (
+                <Button
+                  color="primary"
+                  variant={button.buttonStyle}
+                  component={Link}
+                  to={button.buttonLink}
+                >
+                  {button.buttonText}
+                </Button>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default FeatureCard;
